65fe9442ce03e188f14b06f0e625acaf0d91e444
Darwin Latorre <57970319+darwinlatorre@users.noreply.github.com>
2022-02-17 01:46:36 -0500

    Update Program.cs

--- mvcComputersCompany/Program.cs
+++ mvcComputersCompany/Program.cs
@@ -3,8 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-using System.Data;
-using mvcComputersCompany.logica;
 
 namespace mvcComputersCompany
 {
@@ -16,13 +14,6 @@ namespace mvcComputersCompany
         [STAThread]
         static void Main()
         {
-
-            Portatil varPortatil = new Portatil();
-
-            DataSet varTest = new DataSet();
-
-            varTest = varPortatil.ConsultarPotatilxMarca("Tauret Gaming", "Asus");
-
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new frmMain());
