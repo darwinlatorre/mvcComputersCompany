16f73c5eb9758ec81671133b1a0cb74baee6cbb4
Darwin Latorre <57970319+darwinlatorre@users.noreply.github.com>
2022-02-16 19:31:12 -0500

    Update Script_Procedimientos_Almacenados.sql

--- SQL Scripts/Script_Procedimientos_Almacenados.sql
+++ SQL Scripts/Script_Procedimientos_Almacenados.sql
@@ -23,25 +23,58 @@ END prcRegistrarPortatil;
 
 
 -----------------------------------------------------------------------------------------------------------------------------------
---TESTING PROCEDURE, NOT FINISHED
-CREATE OR REPLACE PROCEDURE prcConsPortatilesMarca(P_NOMBRE IN EMPRESA.EM_NOMBRE%TYPE, P_MARCA IN COMPUTADOR.COM_MARCA%TYPE)
-IS
-    TYPE portatilesMarcaType IS RECORD
-    (
-        COM_NRO_SERIAL COMPUTADOR.COM_NRO_SERIAL%TYPE,
-        COM_CAP_DISCO_DURO_GB COMPUTADOR.COM_CAP_DISCO_DURO_GB%TYPE,
-        COM_CAP_MEMORIA_RAM_GB COMPUTADOR.COM_CAP_MEMORIA_RAM_GB%TYPE
-    );
-    portatilesMarcaRecord portatilesMarcaType;
+--TESTING PROCEDURE, IT NOT FINISHED
 
+-- FIRST PROCEDURE VARIANT
+CREATE OR REPLACE PROCEDURE prcConsPortatilesMarca(P_NOMBRE IN EMPRESA.EM_NOMBRE%TYPE, P_MARCA IN COMPUTADOR.COM_MARCA%TYPE, P_CURSOR_DATOS IN OUT SYS_REFCURSOR)
+AS
 BEGIN
-    SELECT COM_NRO_SERIAL, COM_CAP_DISCO_DURO_GB, COM_CAP_MEMORIA_RAM_GB
-    INTO portatilesMarcaRecord
-    FROM EMPRESA 
-    INNER JOIN COMPUTADOR ON EMPRESA.EM_NIT = COMPUTADOR.EM_NIT
-    WHERE (P_NOMBRE LIKE EMPRESA.EM_NOMBRE AND P_MARCA LIKE COMPUTADOR.COM_MARCA);
+    OPEN P_CURSOR_DATOS FOR 
+        SELECT COM_NRO_SERIAL, COM_CAP_DISCO_DURO_GB, COM_CAP_MEMORIA_RAM_GB
+        FROM EMPRESA 
+        INNER JOIN COMPUTADOR 
+        ON EMPRESA.EM_NIT = COMPUTADOR.EM_NIT
+        WHERE (P_NOMBRE LIKE EMPRESA.EM_NOMBRE AND P_MARCA LIKE COMPUTADOR.COM_MARCA);
 END prcConsPortatilesMarca;
 
-prcConsPortatilesMarca('The Eternal Tao', 'Asus');
+SET SERVEROUTPUT ON
+
+-- TEST prcConsPortatilesMarca 
+DECLARE
+    V_CURSOR SYS_REFCURSOR;
+    v_serial COMPUTADOR.COM_NRO_SERIAL%TYPE;
+    v_cap_disco_duro_gb COMPUTADOR.COM_CAP_DISCO_DURO_GB%TYPE;
+    v_cap_memoria_ram_gb COMPUTADOR.COM_CAP_MEMORIA_RAM_GB%TYPE;
+BEGIN
+    prcConsPortatilesMarca('SpeedLogic', 'Asus', V_CURSOR);
+        LOOP
+            FETCH V_CURSOR 
+            INTO v_serial, v_cap_disco_duro_gb, v_cap_memoria_ram_gb;
+            EXIT WHEN V_CURSOR%NOTFOUND;
+            DBMS_OUTPUT.PUT_LINE(v_serial || ', ' || v_cap_disco_duro_gb || ', ' || v_cap_memoria_ram_gb);
+        END LOOP;
+END;
+
+-- SECOND PROCEDURE VARIANT
+CREATE OR REPLACE PACKAGE pkgPortatilesMarca AS
+    TYPE V_CURSOR_TYPE IS REF CURSOR;
+    PROCEDURE ConsPortatilesMarca(P_NOMBRE IN EMPRESA.EM_NOMBRE%TYPE, P_MARCA IN COMPUTADOR.COM_MARCA%TYPE, P_CURSOR_DATOS IN OUT SYS_REFCURSOR);
+END pkgPortatilesMarca;
+
+CREATE OR REPLACE PACKAGE BODY pkgPortatilesMarca AS
+    PROCEDURE ConsPortatilesMarca(P_NOMBRE IN EMPRESA.EM_NOMBRE%TYPE, P_MARCA IN COMPUTADOR.COM_MARCA%TYPE, P_CURSOR_DATOS IN OUT SYS_REFCURSOR)
+    IS
+        V_CURSOR V_CURSOR_TYPE;
+    BEGIN
+        OPEN V_CURSOR FOR
+        SELECT COM_NRO_SERIAL, COM_CAP_DISCO_DURO_GB, COM_CAP_MEMORIA_RAM_GB
+            FROM EMPRESA
+            INNER JOIN COMPUTADOR
+            ON EMPRESA.EM_NIT = COMPUTADOR.EM_NIT
+            WHERE (P_NOMBRE LIKE EMPRESA.EM_NOMBRE AND P_MARCA LIKE COMPUTADOR.COM_MARCA);
+        P_CURSOR_DATOS := V_CURSOR;
+    END ConsPortatilesMarca;
+END pkgPortatilesMarca;
+
 -----------------------------------------------------------------------------------------------------------------------------------
 
