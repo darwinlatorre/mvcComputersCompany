9b3ebc953991cc4ac331528e1355e368b4841b6b
Juan Ante <juan09ante@gmail.com>
2022-02-21 22:38:11 -0500

    Start of second sprint

---
+++ mvcComputersCompany/frmDiccionarioDatos.Designer.cs
@@ -0,0 +1,76 @@
+﻿
+namespace mvcComputersCompany
+{
+    partial class frmDiccionarioDatos
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnFinalizar = new System.Windows.Forms.Button();
+            this.lblDiccionarioDatos = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnFinalizar
+            // 
+            this.btnFinalizar.Font = new System.Drawing.Font("Cascadia Mono", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnFinalizar.Location = new System.Drawing.Point(522, 12);
+            this.btnFinalizar.Name = "btnFinalizar";
+            this.btnFinalizar.Size = new System.Drawing.Size(100, 30);
+            this.btnFinalizar.TabIndex = 2;
+            this.btnFinalizar.Text = "Finalizar";
+            this.btnFinalizar.UseVisualStyleBackColor = true;
+            this.btnFinalizar.Click += new System.EventHandler(this.btnFinalizar_Click);
+            // 
+            // lblDiccionarioDatos
+            // 
+            this.lblDiccionarioDatos.AutoSize = true;
+            this.lblDiccionarioDatos.Font = new System.Drawing.Font("Cascadia Mono", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblDiccionarioDatos.Location = new System.Drawing.Point(12, 16);
+            this.lblDiccionarioDatos.Name = "lblDiccionarioDatos";
+            this.lblDiccionarioDatos.Size = new System.Drawing.Size(262, 21);
+            this.lblDiccionarioDatos.TabIndex = 3;
+            this.lblDiccionarioDatos.Text = "Diccionario de datos (TO DO)";
+            // 
+            // frmDiccionarioDatos
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(634, 521);
+            this.Controls.Add(this.lblDiccionarioDatos);
+            this.Controls.Add(this.btnFinalizar);
+            this.Name = "frmDiccionarioDatos";
+            this.Text = "Diccionario de datos";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnFinalizar;
+        private System.Windows.Forms.Label lblDiccionarioDatos;
+    }
+}
